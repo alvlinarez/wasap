b8e9c686d8f1c84a80bd5f230166bb9ab4844520
Alvaro Linarez <alvarolinarez9@gmail.com>
2020-09-26 20:53:23 -0500

    React Router removed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
 
 import Home from './containers/Home';
-import NewCategory from './containers/NewCategory';
 import { GlobalStyles } from './styles/GlobalStyles';
 
 export const App = () => {
   return (
-    <BrowserRouter>
+    <Provider store={store}>
       <GlobalStyles />
-      <Provider store={store}>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/new-category" component={NewCategory} />
-        </Switch>
-      </Provider>
-    </BrowserRouter>
+      <Home />
+    </Provider>
   );
 };
